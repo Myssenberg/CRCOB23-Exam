ba2ef1fec5927a5dd9ff975b92d9027fcb2d12cc
My <my.nordaljensen@gmail.com>
2023-04-18 16:50:17 +0200

    Proof of Knowledge done w. comments

--- workspace/ProofOfKnowledge.py
+++ workspace/ProofOfKnowledge.py
@@ -1,46 +1,84 @@
 from petlib import ec
 
+#modeled after "On Sigma Protocols" by Ivan Damgård page 1, first example
+#names should match the naming in the paper.
+
+#Generates a group over an elliptic curve(EC)
+#Takes no arguments
+#Returns group order, q, and group generator, g
 def groupGen():
+    #Generates an elliptic curve group.
+    #p is not needed here when working with EC
     group = ec.EcGroup()
-    order = group.order()
-    g = group.generator()
+    q = group.order() #Order of the group
+    g = group.generator() #Group generator
 
-    return (group, order, g)
+    return q, g
 
-def keygen(order, g):
-    w = order.random() #secret key
-    h = w*g #elliptic curve public key
+
+#Generates secret and public key for Prover using the EC group
+#Takes EC group order, q, and EC group generator, g, as arguments
+#Returns Prover secret key(witness), w, and Prover public key, h.
+def keygen(q, g):
+    w = q.random() #secret key, prover witness
+    h = w*g #EC public key
     return w, h
 
-def Pcommitment(order, g, w):
-    r = order.random()
-    com = r*g
-    return com, r
 
-def Vchallenge(order, g):
-    c = order.random()
+#Generates a Prover commitment (step one in protocol)
+#Takes EC group order, q, and EC group generator, g, as arguments
+#Returns Prover commitment, a, and Prover randomness, r.
+def Prover_commitment(q, g):
+    r = q.random() #Generates randomness for the commitment
+    a = r*g #Computes the commitment
+    return a, r
 
-    return c
 
-def Presponse(r, c, w):
-    z = r + c*w
+#Generates a Verifier challenge (step two in protocol) randomly from the order of the group
+#Takes EC group order, q, as argument
+#Returns Verifier challenge, e.
+def Verifier_challenge(q):
+    e = q.random() #Computes random challenge to send to prover
+
+    return e
+
+
+#Generates Prover response (first part of step three in the protocol)
+#Takes Prover randomness, r, challenge, e, prover witness, w, and EC order, q.
+#Returns Prover response, z.
+def Prover_response(r, e, w, q):
+    z = r + e*w % q #Computes secret response
     return z
 
-def Vverify(g, z, com, h, c):
-    v = z*g == com+c*h
+
+#Verifer Asserts if the ZKP is correct
+#Takes EC group generator(g), response(z), commitment(a), Prover public key(h), challenge(e)
+#Returns either True or False depending on whether the proof goes through.
+def Verifier_verify(g, z, a, h, e):
+    v = z*g == a+e*h #Verifies that the reponse corresponds with the commitment
     return v
 
+
+#Runs a full Proof of Knowledge
 def proof():
-    group, order, g = groupGen()
-    w, h = keygen(order, g)
+    q, g = groupGen() #Generates public knowledge
+    w, h = keygen(q, g) #Prover generates their secret and public keys, "publishing" the public key
 
-    commitment, r = Pcommitment(order, g, w)
+    #Prover generates and "sends" commitment to Verifier.
+    #Notice only Prover knows and keeps r
+    commitment, r = Prover_commitment(q, g)
 
-    challenge = Vchallenge(order, g)
+    #Verifier "receives" commitment. Generates challenge and "sends" it to Prover
+    challenge = Verifier_challenge(q)
 
-    response = Presponse(r, challenge, w)
+    #Prover "receives" challenge. Generates response and "sends" it to Verifier for verification
+    response = Prover_response(r, challenge, w, q)
 
-    verify = Vverify(g, response, commitment, h, challenge)
+    #Verifier "receives" response. Verifies proof with:
+    #Public information: g, h
+    #Verifier generated information: challenge
+    #Information send by prover: commitment, response
+    verify = Verifier_verify(g, response, commitment, h, challenge)
 
     print("Proof verified:", verify)
 
